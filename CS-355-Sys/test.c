d8635b3400c13c79a45f501fa72383aaf5bc8579
Seth <sethwilfinger@gmail.com>
2024-09-09 23:06:00 +0000

    end of lecture

--- CS-355-Sys/test.c
+++ CS-355-Sys/test.c
@@ -1,6 +1,16 @@
 /* cd CCSU_CourseWork/CS-355-Sys */
 #include <stdio.h>
 int main(void){
-    int x;
-    printf("%d\n",x);/* %d for decimal */
+    double x;
+    /*
+     * &: address of _
+    */
+    printf("Enter a character:\n");
+    scanf("%lf",&x);
+    printf("The value of x is %lf \n",x);
+    /*
+     * %d for integers 
+     * %f for floats
+     * %c for characters
+    */
 }
